94735ce378a8b823427a8905e6f02b77bae88855
Omar <omar.branez@gmail.com>
2022-01-09 16:55:35 -0500

    lock size of background image

---
+++ index.js
@@ -0,0 +1,7 @@
+const changeBackgroundImage = () => {
+    document.body.style.backgroundImage = "url('background-corgi.jpg')"
+}
+
+document.addEventListener('DOMContentLoaded', () => {   
+    changeBackgroundImage()
+})
